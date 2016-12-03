711c1e4f5a1baea593f975863119d51e4dea18bd
Michael <mjames.ayres@gmail.com>
2016-12-03 15:47:01 -0500

    Generated my_books controller, added methods

---
+++ wellRead/app/controllers/my_books_controller.rb
@@ -0,0 +1,39 @@
+class MyBooksController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @my_book = MyBook.new
+  end
+
+  def create
+    @my_book = MyBook.new(my_book_params)
+
+    if @my_book.save
+      redirect_to library_url
+    else
+      render :new
+  end
+
+  def show
+  end
+
+  def edit
+    @my_book = MyBook.find(params[:id])
+
+    if @my_book.update_attributes(my_book_params)
+      # insert flash alert that changes have been saved
+      redirect_to :back
+    else
+      redirect_to :back
+    end
+  end
+
+private
+
+  def my_book_params
+    params.require(:user).permit()
+  end
+
+end
--- wellRead/app/controllers/users_controller.rb
+++ wellRead/app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = USer.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       # session[:user_id] = @user.id
