6b88717171f21b8b7c05873a93e7690b6c30ee0d
Michael <mjames.ayres@gmail.com>
2016-12-03 14:07:38 -0500

    Generated Conversations model, migrated db

--- wellRead/db/schema.rb
+++ wellRead/db/schema.rb
@@ -10,15 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203185940) do
+ActiveRecord::Schema.define(version: 20161203190546) do
 
   create_table "books", force: :cascade do |t|
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "libraries", force: :cascade do |t|
   end
 
   create_table "my_books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "review"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
