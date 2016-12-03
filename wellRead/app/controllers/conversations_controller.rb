b27e7fb02cf509a12ffd837db8f023dd26beac95
Michael <mjames.ayres@gmail.com>
2016-12-03 16:02:09 -0500

    Generated conversation controller, added methods

---
+++ wellRead/app/controllers/conversations_controller.rb
@@ -0,0 +1,39 @@
+class ConversationsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @conversations = Conversation.new
+  end
+
+  def create
+    @conversation = Conversation.new(my_book_params)
+
+    if @conversation.save
+      redirect_to root_url
+    else
+      render :new
+  end
+
+  def show
+  end
+
+  def edit
+    @conversation = Conversation.find(params[:id])
+
+    if @conversation.update_attributes(my_book_params)
+      # insert flash alert that changes have been saved
+      redirect_to :back
+    else
+      redirect_to :back
+    end
+  end
+
+private
+
+  def conversation_params
+    params.require(:user).permit()
+  end
+
+end
