4cefa1cbcb8e9b59fb5469258be9f097e2c53324
Michael <mjames.ayres@gmail.com>
2016-12-03 15:38:08 -0500

    Generated user controller, added methods

---
+++ wellRead/app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = USer.new(user_params)
+
+    if @user.save
+      # session[:user_id] = @user.id
+      redirect_to root_url
+    else
+      render :new
+  end
+
+  def show
+    # profile page
+    @user = current_user
+  end
+
+  def edit
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      # insert flash alert that changes have been saved
+      redirect_to :back
+    else
+      redirect_to :back
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:f_name, :l_name, :email, :password, :password_confirmation, :username)
+  end
+
+end
