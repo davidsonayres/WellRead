bc3afd1b2cb644163bdd43eba60b1af8c13dd86e
Michael <mjames.ayres@gmail.com>
2016-12-03 14:18:44 -0500

    Added relationships to User model

--- wellRead/app/models/user.rb
+++ wellRead/app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  has_one :library
+  has_many :my_books through :library
+  has_many :reviews through :my_books
+  # lending
+  # quotes
+  has_many :conversations
+  has_many :chats
+
 end
