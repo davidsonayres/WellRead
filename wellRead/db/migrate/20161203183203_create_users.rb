29d8e8b90dd94f7dbf087fa5022055e16b397e5c
Michael <mjames.ayres@gmail.com>
2016-12-03 13:38:35 -0500

    Rails new, generated User model, migrated db

---
+++ wellRead/db/migrate/20161203183203_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :f_name
+      t.string :l_name
+      t.string :email
+      t.string :password_digest
+      t.string :username
+
+      t.timestamps
+    end
+  end
+end
