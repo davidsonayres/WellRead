616179a95f3cacade3f665ff72674d62a3df45fd
Michael <mjames.ayres@gmail.com>
2016-12-03 13:41:12 -0500

    Generated Libraries model, migrated db

---
+++ wellRead/db/migrate/20161203183925_create_libraries.rb
@@ -0,0 +1,7 @@
+class CreateLibraries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :libraries do |t|
+
+    end
+  end
+end
--- wellRead/db/schema.rb
+++ wellRead/db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203183203) do
+ActiveRecord::Schema.define(version: 20161203183925) do
+
+  create_table "libraries", force: :cascade do |t|
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "f_name"
