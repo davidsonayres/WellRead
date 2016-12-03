6fbd33eba437c3d1607d0b009361e132c7e86dc4
Meg Clarke <meggyclarke@gmail.com>
2016-12-03 16:31:59 -0500

    Added Session Controller and Methods to it.

---
+++ wellRead/app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+          redirect_to :back
+      else
+        redirect_to root_url
+      end
+
+    end
+
+    def destroy
+      session[:user_id] = nil
+      redirect_to root_url
+    end
+
+    private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
+end
