3dac55857edb0b536f6ba651d127432c9400aad0
Michael <mjames.ayres@gmail.com>
2016-12-06 15:48:07 -0500

    Michael installed figaro gem

--- wellRead/db/schema.rb
+++ wellRead/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161203191045) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
   end
 
