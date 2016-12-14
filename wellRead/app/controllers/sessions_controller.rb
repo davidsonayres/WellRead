4edc46bdde62cfdba503a48bd4d6d89b243915d1
Meg Clarke <meggyclarke@gmail.com>
2016-12-13 21:10:58 -0500

    Created a home page for sign in or sign up. Adjusted sessions and routes and added links.

--- wellRead/app/controllers/sessions_controller.rb
+++ wellRead/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-          redirect_to library_path
+        redirect_to libraries_path
       else
         redirect_to root_url
       end
--- wellRead/config/routes.rb
+++ wellRead/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root 'users#new'
+root 'users#index'
 
 resources :my_books do
     resources :reviews
@@ -22,5 +22,6 @@ post '/booksearch', to: 'books#searchtobook'
 post '/my_booksearch', to: 'my_books#searchtomybook'
 post '/edition', to: 'edition#create'
 post '/chats/new', to: 'chats#create'
+
 
 end
