2144b35fe5f54fa8a3e620d8f9ccf802d19db83c
Meg Clarke <meggyclarke@gmail.com>
2016-12-21 18:03:42 -0500

    Lending now able to lend out a book and see it on the lend view

---
+++ wellRead/app/controllers/lends_controller.rb
@@ -0,0 +1,38 @@
+class LendsController < ApplicationController
+    def index
+        @lends = Lend.all
+          @lend = Lend.new(lend_params)
+          @user = current_user
+          @my_book = MyBook.where(my_book_id: params[:my_book_id])
+
+    end
+
+    def new
+    @lend = Lend.new
+    @my_book = MyBook.find(params[:my_book_id])
+    @lend.my_book_id = params[:my_book_id]
+    end
+
+    def create
+      @user = current_user
+      @my_book = MyBook.where(user_id: @user.id)
+      @lends = Lend.where(user_id: @user.id)
+      @lend = Lend.new(lend_params)
+      @lend.my_book = MyBook.find(params[:my_book_id])
+
+    if @lend.save!
+      redirect_to my_book_url(params[:my_book_id])
+
+    else
+      render :new
+
+    end
+    end
+
+    private
+
+    def lend_params
+    params.permit( :my_book_id, :friend)
+    end
+
+    end
