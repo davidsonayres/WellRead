d47b55d06377488ab8e445fd0001639ffa9d72db
Meg Clarke <meggyclarke@gmail.com>
2016-12-30 12:43:48 -0500

    Added Lend code to be able to display name of lent to. Code is currently commented out until the lent id can be passed through the mybooks controller.

--- wellRead/app/controllers/lends_controller.rb
+++ wellRead/app/controllers/lends_controller.rb
@@ -1,34 +1,36 @@
 class LendsController < ApplicationController
     def index
         @lends = Lend.all
-          @lend = Lend.new(lend_params)
-          @user = current_user
-          @my_book = MyBook.find(params[:my_book_id])
-          @borrows = Borrow.all
-          @borrow = Borrow.new(borrow_params)
-
+        @lend = Lend.new(lend_params)
+        @user = current_user
+        @my_book = MyBook.find(params[:my_book_id])
+        @borrows = Borrow.all
+        @borrow = Borrow.new(borrow_params)
     end
 
     def new
-    @lend = Lend.new
-    @my_book = MyBook.find(params[:my_book_id])
-    @lend.my_book_id = params[:my_book_id]
+        @lend = Lend.new
+        @my_book = MyBook.find(params[:my_book_id])
+        @lend.my_book_id = params[:my_book_id]
     end
 
     def create
-      @user = current_user
-      @my_book = MyBook.where(user_id: @user.id)
-      @lends = Lend.where(user_id: @user.id)
-      @lend = Lend.new(lend_params)
-      @lend.my_book = MyBook.find(params[:my_book_id])
+        @user = current_user
+        @my_book = MyBook.where(user_id: @user.id)
+        @lends = Lend.where(user_id: @user.id)
+        @lend = Lend.new(lend_params)
+        @lend.my_book = MyBook.find(params[:my_book_id])
 
         if @lend.save!
-          redirect_to my_book_url(params[:my_book_id])
-
+            redirect_to my_book_url(params[:my_book_id])
         else
-          render :new
-
+            render :new
         end
+    end
+
+    def destroy
+        @lend.destroy
+        redirect_to my_book_path(@my_book)
     end
 
     private
@@ -37,4 +39,4 @@ class LendsController < ApplicationController
     params.permit( :my_book_id, :friend)
     end
 
-    end
+end
--- wellRead/app/controllers/my_books_controller.rb
+++ wellRead/app/controllers/my_books_controller.rb
@@ -1,4 +1,5 @@
 class MyBooksController < ApplicationController
+    #before_action :find_lend, only: [:show]
 
   def index
     @user = current_user
@@ -32,76 +33,79 @@ class MyBooksController < ApplicationController
     @quotes = Quote.where(user_id: @user.id)
   end
 
-  def edit
+    def edit
     @my_book = MyBook.find(params[:id])
-
-    if @my_book.update_attributes(my_book_params)
-      # insert flash alert that changes have been saved
-      redirect_to :back
-    else
-      redirect_to :back
+        if @my_book.update_attributes(my_book_params)
+            # insert flash alert that changes have been saved
+            redirect_to :back
+        else
+            redirect_to :back
+        end
     end
-  end
 
-  def searchtomybook
-      @my_book = MyBook.find_by edition_id: params["edition_id"]
-      if @my_book == nil
-        @edition = Edition.find_by title: params["title"],author: params["author"]
-          if @edition == nil
-              @book = Book.find_by title: params["title"], author: params["author"]
-              if @book == nil #no my book, no edition, no book
-                  @book = Book.new(book_params)
-                  @book.save!
+    def searchtomybook
+    @my_book = MyBook.find_by edition_id: params["edition_id"]
+        if @my_book == nil
+            @edition = Edition.find_by title: params["title"],author: params["author"]
+            if @edition == nil
+                @book = Book.find_by title: params["title"], author: params["author"]
+                if @book == nil #no my book, no edition, no book
+                    @book = Book.new(book_params)
+                    @book.save!
 
-                  @edition = Edition.new(edition_params)
-                  @edition.book_id = @book.id
-                  @edition.save!
+                    @edition = Edition.new(edition_params)
+                    @edition.book_id = @book.id
+                    @edition.save!
 
-                  @my_book = MyBook.new(my_book_params)
-                  @my_book.edition_id = @edition.id
-                  @my_book.save!
+                    @my_book = MyBook.new(my_book_params)
+                    @my_book.edition_id = @edition.id
+                    @my_book.save!
 
-              else #book exists, no edition, no mybook
-                  @edition = Edition.new(edition_params)
-                  @edition.book_id = @book.id
-                  @edition.save!
+                else #book exists, no edition, no mybook
+                    @edition = Edition.new(edition_params)
+                    @edition.book_id = @book.id
+                    @edition.save!
 
-                  @my_book = MyBook.new(my_book_params)
-                  @my_book.edition_id = @edition.id
-                  @my_book.save!
+                    @my_book = MyBook.new(my_book_params)
+                    @my_book.edition_id = @edition.id
+                    @my_book.save!
 
                 end #end of if book doesn't exist
 
             end # end of if edition doesn't exist
 
-          @my_book = MyBook.new(my_book_params)
-          @my_book.edition_id = @edition.id
-          @my_book.save!
+        @my_book = MyBook.new(my_book_params)
+        @my_book.edition_id = @edition.id
+        @my_book.save!
 
-      end #end of if my book doesn't exist
-      unless MyBook.find_by(user_id:current_user.id, edition_id:@edition.id)
+        end #end of if my book doesn't exist
+        unless MyBook.find_by(user_id:current_user.id, edition_id:@edition.id)
 
-          @my_book.save!
-          @my_books = MyBook.where(user_id: current_user.id)
+            @my_book.save!
+            @my_books = MyBook.where(user_id: current_user.id)
             redirect_to my_book_path(@my_book.id)
-      else
-          @user = current_user
-          @my_books = MyBook.where(user_id: @user.id)
-        redirect_to my_book_path(@my_book.id)
-      end
-  end #end of method
+        else
+            @user = current_user
+            @my_books = MyBook.where(user_id: @user.id)
+            redirect_to my_book_path(@my_book.id)
+        end
+    end #end of method
 
 private
 
-  def my_book_params
-    params.permit(:edition_id)
-  end
+    def my_book_params
+        params.permit(:edition_id)
+    end
 
-  def book_params
+    def book_params
       params.permit(:title, :author)
-  end
+    end
 
-  def edition_params
-      params.permit(:book_id, :title, :author, :edition, :genre, :numberOfPages, :publicationDate, :publisher, :url, :image)
-  end
+    def edition_params
+        params.permit(:book_id, :title, :author, :edition, :genre, :numberOfPages, :publicationDate, :publisher, :url, :image)
+    end
+
+    # def find_lend
+    #     @lend = Lend.find(params[:id])
+    # end
 end #end of Class
