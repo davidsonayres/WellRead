0c1b96bbf30c98f28174133729d8861da91f870e
Michael <mjames.ayres@gmail.com>
2017-01-05 13:50:21 -0500

    Installed minimagick gem, enabled it in quote uploader, adjusted quote index to display uploaded quote in div - step 1 for quote highlighting feature

--- wellRead/app/uploaders/quote_uploader.rb
+++ wellRead/app/uploaders/quote_uploader.rb
@@ -2,7 +2,9 @@ class QuoteUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [800,800]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
