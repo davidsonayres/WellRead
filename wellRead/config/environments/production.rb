64cf210a0cb2bed9bc16b874b4915b4d5501e369
Meg Clarke <meggyclarke@gmail.com>
2017-01-05 14:14:34 -0500

    added config.assets.compile = false

--- wellRead/config/environments/production.rb
+++ wellRead/config/environments/production.rb
@@ -17,7 +17,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+# ^^^ Meg commented out
+config.serve_static_files = true
+#^^^ Meg input to see if this will resolve the Ratings Stars showing up
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
