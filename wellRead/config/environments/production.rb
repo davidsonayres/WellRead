f6be86f486c19d960f31940f14b95e7b0a0be375
Meg Clarke <meggyclarke@gmail.com>
2017-01-06 16:24:33 -0500

    getting stars to work

--- wellRead/config/environments/production.rb
+++ wellRead/config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   config.assets.compile = true
   config.serve_static_assets = true
   config.assets.digest = true
-  # config.serve_static_files = true
+  config.serve_static_files = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
