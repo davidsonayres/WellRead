48140cb9ffe204c18c009116ebd8ff92676118dd
Meg Clarke <meggyclarke@gmail.com>
2017-01-06 17:25:09 -0500

    Fixed syntax error on Editions show page

--- wellRead/app/assets/javascripts/special.js
+++ wellRead/app/assets/javascripts/special.js
@@ -17,4 +17,12 @@
         // starHalf: image_path("images/star-half.png")
         // starOn: image_path("images/star-on.png")
     });
+
+    $('#average-rating').raty({
+        readOnly:true,
+        score: function(){
+            return $(this).attr('data-score');
+        },
+        path: '/assets/'
+    });
 })();
