ab46c3e65f514032c55f19acffdf590021beae53
Meg Clarke <meggyclarke@gmail.com>
2017-01-07 16:59:22 -0500

    Moving JS script tags back onto show pages for stars

--- wellRead/app/assets/javascripts/special.js
+++ wellRead/app/assets/javascripts/special.js
@@ -1,28 +1,28 @@
-(function() {
-    $('#rating-form').raty({
-        path: '/assets/',
-        scoreName: 'rating[rating]',
-        // score: "1"
-    });
-
-
-
-    $('#rating').raty({
-        readOnly:true,
-        score: function(){
-            return $(this).attr('data-score');
-        },
-        path: '/assets/'
-        // starOff: image_path("images/star-off.png")
-        // starHalf: image_path("images/star-half.png")
-        // starOn: image_path("images/star-on.png")
-    });
-
-    $('#average-rating').raty({
-        readOnly:true,
-        score: function(){
-            return $(this).attr('data-score');
-        },
-        path: '/assets/'
-    });
-})();
+// (function() {
+//     $('#rating-form').raty({
+//         path: '/assets/',
+//         scoreName: 'rating[rating]',
+//         // score: "1"
+//     });
+//
+//
+//
+//     $('#rating').raty({
+//         readOnly:true,
+//         score: function(){
+//             return $(this).attr('data-score');
+//         },
+//         path: '/assets/'
+//         // starOff: image_path("images/star-off.png")
+//         // starHalf: image_path("images/star-half.png")
+//         // starOn: image_path("images/star-on.png")
+//     });
+//
+//     $('#average-rating').raty({
+//         readOnly:true,
+//         score: function(){
+//             return $(this).attr('data-score');
+//         },
+//         path: '/assets/'
+//     });
+// })();
