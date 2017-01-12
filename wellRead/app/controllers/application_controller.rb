afacb7a05b90b08ea20609be583b4dd031d7387a
Meg Clarke <meggyclarke@gmail.com>
2017-01-12 16:37:21 -0500

    Removed public assets

--- wellRead/app/controllers/application_controller.rb
+++ wellRead/app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
 
-     protect_from_forgery with: :exception
+      protect_from_forgery with: :exception
     #protect_from_forgery with: :null_session
+    #protect_from_forgery prepend: true
 
    private
 
