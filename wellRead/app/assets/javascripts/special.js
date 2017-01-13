1207238be31cea3b2ee12b23125739d195536afe
Meg Clarke <meggyclarke@gmail.com>
2017-01-12 23:03:28 -0500

    Adding Function to each script

--- wellRead/app/assets/javascripts/special.js
+++ wellRead/app/assets/javascripts/special.js
@@ -1,28 +1,28 @@
-(function() {
-    $('#rating-form').raty({
-            path: 'public/images/stars/'
-        scoreName: 'rating[rating]',
-        // score: "1"
-    });
-
-
-
-    $('#rating').raty({
-        readOnly:true,
-        score: function(){
-            return $(this).attr('data-score');
-        },
-            path: 'public/images/stars/'
-        // starOff: image_path("images/star-off.png")
-        // starHalf: image_path("images/star-half.png")
-        // starOn: image_path("images/star-on.png")
-    });
-
-    $('#average-rating').raty({
-        readOnly:true,
-        score: function(){
-            return $(this).attr('data-score');
-        },
-            path: 'public/images/stars/'
-    });
-})();
+// (function() {
+//     $('#rating-form').raty({
+//             path: 'public/images/stars/'
+//         scoreName: 'rating[rating]',
+//         // score: "1"
+//     });
+//
+//
+//
+//     $('#rating').raty({
+//         readOnly:true,
+//         score: function(){
+//             return $(this).attr('data-score');
+//         },
+//             path: 'public/images/stars/'
+//         // starOff: image_path("images/star-off.png")
+//         // starHalf: image_path("images/star-half.png")
+//         // starOn: image_path("images/star-on.png")
+//     });
+//
+//     $('#average-rating').raty({
+//         readOnly:true,
+//         score: function(){
+//             return $(this).attr('data-score');
+//         },
+//             path: 'public/images/stars/'
+//     });
+// })();
