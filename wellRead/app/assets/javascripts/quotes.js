8203126a1935c19c41472c34c050f91eed66a6f1
Michael <mjames.ayres@gmail.com>
2017-01-14 16:23:08 -0500

    Touch events added for quote highlight, quote show page styled, minor adjustments to spacing on layout html page

--- wellRead/app/assets/javascripts/quotes.js
+++ wellRead/app/assets/javascripts/quotes.js
@@ -8,6 +8,7 @@
   canvas.height = parseInt(sketch_style.getPropertyValue('height'));
 
   var mouse = {x: 0, y: 0};
+  var touch = {x: 0, y: 0};
 
   /* Mouse Capturing Work */
   canvas.addEventListener('mousemove', function(e) {
@@ -37,11 +38,91 @@
       ctx.stroke();
   };
 
+  function drawLine(ctx,x,y,size) {
+
+        // If lastX is not set, set lastX and lastY to the current position
+        if (lastX==-1) {
+            lastX=x;
+      lastY=y;
+        }
+        ctx.lineJoin = 'round';
+        // Select a fill style
+        ctx.strokeStyle = "yellow";
+
+        // Set the line "cap" style to round, so lines at different angles can join into each other
+        ctx.lineCap = "round";
+        //ctx.lineJoin = "round";
+
+
+        // Draw a filled line
+        ctx.beginPath();
+
+  // First, move to the old (previous) position
+  ctx.moveTo(lastX,lastY);
+
+  // Now draw a line to the current touch/pointer position
+  ctx.lineTo(x,y);
+
+        // Set the line thickness and draw the line
+        ctx.lineWidth = 30;
+        ctx.stroke();
+
+        ctx.closePath();
+
+  // Update the last position to reference the current position
+  var lastX = x;
+  var lastY = y;
+    }
+
+  function highlight_touchStart() {
+      // Update the touch co-ordinates
+      getTouchPos();
+
+      drawLine(ctx,touchX,touchY,6);
+
+      // Prevents an additional mousedown event being triggered
+      event.preventDefault();
+  }
+
+  // Draw something and prevent the default scrolling when touch movement is detected
+  function highlight_touchMove(e) {
+      // Update the touch co-ordinates
+      getTouchPos(e);
+
+      // During a touchmove event, unlike a mousemove event, we don't need to check if the touch is engaged, since there will always be contact with the screen by definition.
+      drawLine(ctx,touchX,touchY,6);
+
+      // Prevent a scrolling action as a result of this touchmove triggering.
+      event.preventDefault();
+  }
+
+  // Get the touch position relative to the top-left of the canvas
+  // When we get the raw values of pageX and pageY below, they take into account the scrolling on the page
+  // but not the position relative to our target div. We'll adjust them using "target.offsetLeft" and
+  // "target.offsetTop" to get the correct values in relation to the top left of the canvas.
+  function getTouchPos(e) {
+      if (!e)
+          var e = event;
+
+      if(e.touches) {
+          if (e.touches.length == 1) { // Only deal with one finger
+              var touch = e.touches[0]; // Get the information for finger #1
+              touchX=touch.pageX-touch.target.offsetLeft;
+              touchY=touch.pageY-touch.target.offsetTop;
+          }
+      }
+  }
+
   /* Saving Drawn Image as DataURL */
 
   $("#highlight_form").submit(function() {
     var dataURL = canvas.toDataURL();
     $("#quote_highlight").attr("value", dataURL);
   });
+
+  // React to touch events on the canvas
+  canvas.addEventListener('touchstart', highlight_touchStart, false);
+  canvas.addEventListener('touchmove', highlight_touchMove, false);
+
 
 }());
